10a4c3e9456ce21bfebce604f37d689284a3516e
Tomas Jungman <info@teejays.cz>
2016-04-19 19:03:56 +0200

    webmin install, fully qualified dir names

--- webmin.pp
+++ webmin.pp
@@ -1,15 +1,15 @@
 exec { 'update_apt':
   command => 'apt-get update',
-  cwd => "~",
+  cwd => "/root/",
 }
 ->
 exec { 'resolve_webmin_dependencies':
   command => 'apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python',
-  cwd => "~",
+  cwd => "/root/",
 }
 ->
 exec { 'download_webmin_install':
   command => 'wget http://prdownloads.sourceforge.net/webadmin/webmin_1.791_all.deb',
-  cwd => "~",
+  cwd => "/root/",
 }
 
