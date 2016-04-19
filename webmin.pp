c2042ddc467374b92e2820856a3945be22d18326
Tomas Jungman <info@teejays.cz>
2016-04-19 19:08:11 +0200

    webmin install, path added

--- webmin.pp
+++ webmin.pp
@@ -1,15 +1,18 @@
 exec { 'update_apt':
   command => 'apt-get update',
   cwd => "/root/",
+  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 ->
 exec { 'resolve_webmin_dependencies':
   command => 'apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python',
   cwd => "/root/",
+  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 ->
 exec { 'download_webmin_install':
   command => 'wget http://prdownloads.sourceforge.net/webadmin/webmin_1.791_all.deb',
   cwd => "/root/",
+  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
 
