bd5f607173c90bdafd95a25755f80659863bc940
Tomas Jungman <info@teejays.cz>
2016-04-19 18:59:49 +0200

    install webmin, fixed workdir param

--- webmin.pp
+++ webmin.pp
@@ -1,15 +1,15 @@
 exec { 'update_apt':
   command => 'apt-get update',
-  path => "~",
+  cwd => "~",
 }
 ->
 exec { 'resolve_webmin_dependencies':
   command => 'apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python',
-  path => "~",
+  cwd => "~",
 }
 ->
 exec { 'download_webmin_install':
   command => 'wget http://prdownloads.sourceforge.net/webadmin/webmin_1.791_all.deb',
-  path => "~",
+  cwd => "~",
 }
 
